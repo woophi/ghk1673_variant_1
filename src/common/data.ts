beb4c4c763fd42d4f3dcf8a4036d6e777b714857
Konstantin Mikheev <mik.kon6610@gmail.com>
2024-02-15 09:19:21 +0100

    init ghk1673_variant_1

---
+++ src/common/data.ts
@@ -0,0 +1,99 @@
+import dostLogo from '../assets/dost_logo.png';
+import hhLogo from '../assets/hh_logo.png';
+import inviLogo from '../assets/invi_logo.png';
+import lavLogo from '../assets/lav_logo.png';
+import lvlLogo from '../assets/lvl_logo.png';
+import mosbLogo from '../assets/mosb_logo.png';
+import muzLogo from '../assets/muz_logo.png';
+import premierLogo from '../assets/premier_logo.png';
+import questLogo from '../assets/quest_logo.png';
+import steakLogo from '../assets/steak_logo.png';
+import talonLogo from '../assets/talon_logo.png';
+import yaLogo from '../assets/ya_logo.png';
+
+export const data = [
+  {
+    logo: yaLogo,
+    title: 'Яндекс.Маркет',
+    category: 'Маркетплейс',
+    subcategory: 'До 29.12.2024',
+    percentage: 'до 30%',
+  },
+  {
+    logo: talonLogo,
+    title: 'Эталон',
+    category: 'Строительная компания',
+    subcategory: 'До 31.03.2024',
+    percentage: 'до 50%',
+  },
+  {
+    logo: premierLogo,
+    title: 'PREMIER',
+    category: 'Онлайн-кинотеатр',
+    subcategory: 'До 31.03.2024',
+    percentage: 'до 40%',
+  },
+  {
+    logo: steakLogo,
+    title: 'Стейк & Бургер Мираторг',
+    category: 'Сеть бургерных',
+    subcategory: 'До 28.02.2024',
+    percentage: '15%',
+  },
+  {
+    logo: muzLogo,
+    title: 'MIUZ Diamonds',
+    category: 'Ювелирный завод',
+    subcategory: 'До 31.03.2024',
+    percentage: 'до 10%',
+  },
+  {
+    logo: mosbLogo,
+    title: 'Moscow Business School',
+    category: 'Образование',
+    subcategory: 'До 29.02.2024',
+    percentage: '15%',
+  },
+  {
+    logo: inviLogo,
+    title: 'Инвитро',
+    category: 'Медицина',
+    subcategory: 'До 31.12.2024',
+    percentage: '10%',
+  },
+  {
+    logo: hhLogo,
+    title: 'hh.ru',
+    category: 'Онлайн-рекрутинг',
+    subcategory: 'До 05.03.2024',
+    percentage: 'до 20%',
+  },
+  {
+    logo: dostLogo,
+    title: 'Dostavista',
+    category: 'Курьерская доставка',
+    subcategory: 'До 29.02.2024',
+    percentage: 'до 20%',
+  },
+  {
+    logo: lavLogo,
+    title: 'Яндекс.Лавка',
+    category: 'Продукты с доствкой',
+    subcategory: 'До 29.02.2024',
+    percentage: '15%',
+  },
+  {
+    logo: lvlLogo,
+    title: 'Level Kitchen',
+    category: 'Продукты с доствкой',
+    subcategory: 'До 30.04.2024',
+    percentage: '15%',
+  },
+  {
+    logo: questLogo,
+    title: 'Quest Reality',
+    category: 'Квесты в реальности',
+    subcategory: 'До 29.02.2024',
+    percentage: 'до 20%',
+  },
+];
